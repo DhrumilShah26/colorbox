1ea6d27df386c36e1cd2662f847d52e1a9d84fcc
Dhrumil <sdhrumil2605@gmail.com>
2021-05-26 22:29:01 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: "100",
+      width: "50",
+      backgroundColor: "#000000"
+    };
+
+    this.styles = {
+      height: "100px",
+      width: "50px",
+      backgroundColor: "#000000"
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.styles = {
+      ...this.styles,
+      ...this.state
+    };
+
+    this.styles.width = this.state.width + "px";
+    this.styles.height = this.state.height + "px";
+
+    this.forceUpdate(() => {
+      console.log("Forcing Update the App.")
+    });
+  };
+
+  render() {
+    return (
+      <div className="App container">
+        <form onSubmit={this.handleSubmit}>
+          <label>Height: </label>
+          <input
+            type="number"
+            value={this.state.height}
+            name="height"
+            onChange={this.handleChange}
+            min="1"
+          />
+          <label>Width: </label>
+          <input
+            type="number"
+            value={this.state.width}
+            name="width"
+            onChange={this.handleChange}
+            min="1"
+          />
+          <label>Color: </label>
+          <input
+            type="color"
+            value={this.state.backgroundColor}
+            name="backgroundColor"
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="submit" />
+        </form>
+        <div style={this.styles} />
+      </div>
+    );
+  }
 }
 
 export default App;
